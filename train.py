6e69e31b30363676b6e51f3ff83102be88144571
Thomas Whitington <thomaswhitington@gmail.com>
2021-04-26 11:13:31 +0200

    log_dir not logdir

--- train.py
+++ train.py
@@ -125,7 +125,7 @@ def set_grad_none(model, targets):
 
 
 def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, device):
-    writer = SummaryWriter(logdir=args.tensorboard_dir)
+    writer = SummaryWriter(log_dir=args.tensorboard_dir)
     writer.add_graph(generator)
     loader = sample_data(loader)
 
